c64169c694e1083c2083b679842c9326e0fd1bc9
noacaspi <nocaspi@mail.tau.ac.il>
2025-06-25 22:53:46 +0300

    honeycomb

--- Codes.py
+++ Codes.py
@@ -2,6 +2,8 @@
 Implementation of "Deep Quantum Error Correction" (DQEC), AAAI24
 @author: Yoni Choukroun, choukroun.yoni@gmail.com
 """
+from functools import lru_cache
+
 import numpy as np
 import torch
 from scipy.sparse import hstack, kron, eye, csr_matrix, block_diag
@@ -103,112 +105,185 @@ class ToricCode:
 
 class Honeycomb666Code:
     '''
-    Honeycomb 6-6-6 code implementation on a closed (torus) layout
+    Honeycomb 6-6-6 code implementation on a triangle layout
     
-    The honeycomb lattice is constructed as a hexagonal grid where:
+    The honeycomb lattice is constructed as a hexagonal grid inside a triangle.
     - Physical qubits are placed on the vertices
-    - Each hexagon (face) corresponds to a stabilizer generator
-    
-    Hexagonal lattice structure:
-           q0 - q1
-          /       \
-        q5    H    q2
-          \       /
-           q4 - q3
-    
-    Where H is a hexagonal face (stabilizer) and q0-q5 are qubits on the edges.
+    - Each hexagon (face) corresponds to a stabilizer generator (both for X and Z)
+    - Logical operators are defined by an edge of the triangle
 
-    Each hexagon is a stabilizer for both X and Z errors.
+    Hexagonal lattice structure:
+
+                   q0
+                 /   \
+                /     q1
+               /     /  \
+              q2 - q3    \
+             /       \    \
+            q4       q5 - q6
+          /  \       /      \
+         /    q7 - q8       q9
+        /     /      \      /  \
+       q10 - q11     q12 - q13  \
+      /       \      /       \   \
+    q14  - - q15 - q16 -  - q17 - q18
+
+    The first level is qubit 0 (1 qubits)
+    The second level is qubit 1 (1 qubits)
+    The third level are qubits 2-3 (2 qubits)
+    The fourth level are qubits 4-6 (3 qubits)
+    The fifth level are qubits 7-9 (3 qubits)
+    The sixth level are qubits 10-13 (4 qubits)
+    The seventh level are qubit 14-18 (5 qubits)
+    Where H is a hexagonal face (stabilizer) and q0-q18 are qubits on the edges.
     '''
     
     def __init__(self, L):
         '''
-        Honeycomb 6-6-6 code with L hexagons on one side of the lattice
+        Honeycomb 6-6-6 code with L qubits on one edge of the triangle.
         
         Args:
-            L: Linear size parameter (actual number of qubits will be larger)
+            L: Linear size parameter, number of qubits on one side of the triangle.
         '''
-        self.L = L # number of hexagons on one side of the lattice
-        self.W = (L + 1) // 2 * 2 # width of the lattice (even number)
-        self.n_qubits = 2 * self.L * self.W # total number of qubits
-        self.n_hexagons = self.L * self.W # total number of hexagons
+        assert(L % 2 == 1), "L must be odd"  # not sure how to handle even L
+        self.L = L # number of qubits on one side of the triangle
+        self.n_qubits = self._n_qubits_by_L(L) # total number of qubits
+        self.n_level = self._n_levels_by_L(L) # number of levels in the triangle
+        assert(self.n_qubits == self._n_qubits_until_level(self.n_level))
 
-    @property
-    def flatXflips2Zstab(self):
-        stab = np.zeros((self.n_hexagons, self.n_qubits), dtype=np.dtype('b'))
-        for i, j in itertools.product(range(self.L),range(self.W)):
-            # i is the row, j is the column
-            # on an even column (starting from 0), the first edge of the hexagon is (2*i, j)
-            # and on an odd column, the first edge is (2*i+1,j)
-            start_row = 2 * i if j%2 == 0 else 2*i+1
-            middle_row = (start_row + 1) % (2 * self.L)
-            end_row = (start_row + 2) % (2 * self.L)
-            start_column = j
-            end_column = (j + 1) % self.W
-            stab[i*self.W+j, start_row * self.W + start_column] = 1
-            stab[i*self.W+j, middle_row * self.W + start_column] = 1
-            stab[i*self.W+j, end_row * self.W + start_column] = 1
-            stab[i*self.W+j, start_row * self.W + end_column] = 1
-            stab[i*self.W+j, middle_row * self.W + end_column] = 1
-            stab[i*self.W+j, end_row * self.W + end_column] = 1
-        return stab
+    @staticmethod
+    def _n_qubits_by_L(L):
+        return int(3/4 * (L**2) + 1/4)
 
-    @property
-    def flatZflips2Xstab(self):
-        stab = np.zeros((self.n_hexagons, self.n_qubits), dtype=np.dtype('b'))
-        for i, j in itertools.product(range(self.L),range(self.W)):
-            # i is the row, j is the column
-            # on an even column (sarting from 0), the first edge of the hexagon is (2*i, j)
-            # and on an odd column, the first edge is (2*i+1,j)
-            start_row = 2 * i if j%2 == 0 else 2*i+1
-            middle_row = (start_row + 1) % (2 * self.L)
-            end_row = (start_row + 2) % (2 * self.L)
-            start_column = j
-            end_column = (j + 1) % self.W
-            stab[i*self.W+j, start_row * self.W + start_column] = 1
-            stab[i*self.W+j, middle_row * self.W + start_column] = 1
-            stab[i*self.W+j, end_row * self.W + start_column] = 1
-            stab[i*self.W+j, start_row * self.W + end_column] = 1
-            stab[i*self.W+j, middle_row * self.W + end_column] = 1
-            stab[i*self.W+j, end_row * self.W + end_column] = 1
-        return stab
+    @staticmethod
+    def _n_levels_by_L(L):
+        return int(1.5 * L - 0.5)
+
+    @lru_cache(maxsize=None)
+    def _n_qubits_in_level(self, level):
+        return (2 * level + 3) // 3
+
+    @lru_cache(maxsize=None)
+    def _n_qubits_until_level(self, level):
+        return sum(self._n_qubits_in_level(l) for l in range(level))
+
+    def _qubit_index(self, level, j):
+        if j >= 0:
+            return self._n_qubits_until_level(level) + j
+        else:
+            # j is negative, we are counting from the end of the level
+            return self._n_qubits_until_level(level + 1) + j
+
+    def _is_qubit_on_right_edge_in_level(self, level):
+        return level % 3 != 1
+
+    def _stabilizers(self):
+        stabilizers = []
+        # Red stabilizers
+        for level in range(0, self.n_level - 2, 3):
+            # stabilizer on the right edge
+            stab = np.zeros((self.n_qubits,), dtype=np.dtype('b'))
+            stab[self._qubit_index(level, 0)] = 1
+            stab[self._qubit_index(level + 1, 0)] = 1
+            stab[self._qubit_index(level + 2, 0)] = 1
+            stab[self._qubit_index(level + 2, 1)] = 1
+            stabilizers.append(stab)
+
+            for i in range(1, self._n_qubits_in_level(level), 2):
+                # stabilizer in the middle
+                stab = np.zeros((self.n_qubits,), dtype=np.dtype('b'))
+                stab[self._qubit_index(level, i)] = 1
+                stab[self._qubit_index(level, i + 1)] = 1
+                stab[self._qubit_index(level + 1, i)] = 1
+                stab[self._qubit_index(level + 1, i + 1)] = 1
+                stab[self._qubit_index(level + 2, i + 1)] = 1
+                stab[self._qubit_index(level + 2, i + 2)] = 1
+                stabilizers.append(stab)
+
+        # Blue stabilizers
+        for level in range(1, self.n_level - 2, 3):
+            # stabilizer on the left edge
+            stab = np.zeros((self.n_qubits,), dtype=np.dtype('b'))
+            stab[self._qubit_index(level, -1)] = 1
+            stab[self._qubit_index(level + 1, -1)] = 1
+            stab[self._qubit_index(level + 2, -1)] = 1
+            stab[self._qubit_index(level + 2, -2)] = 1
+            stabilizers.append(stab)
+
+            for i in range(0, self._n_qubits_in_level(level)-2, 2):
+                # stabilizer in the middle
+                stab = np.zeros((self.n_qubits,), dtype=np.dtype('b'))
+                stab[self._qubit_index(level, i)] = 1
+                stab[self._qubit_index(level, i + 1)] = 1
+                stab[self._qubit_index(level + 1, i + 1)] = 1
+                stab[self._qubit_index(level + 1, i + 2)] = 1
+                stab[self._qubit_index(level + 2, i + 1)] = 1
+                stab[self._qubit_index(level + 2, i + 2)] = 1
+                stabilizers.append(stab)
+
+        # Green stabilizers
+        # bottom stabilizers
+        for i in range(0, self._n_qubits_in_level(self.n_level - 2) - 1, 2):
+            stab = np.zeros((self.n_qubits,), dtype=np.dtype('b'))
+            stab[self._qubit_index(self.n_level - 1, i)] = 1
+            stab[self._qubit_index(self.n_level - 1, i + 1)] = 1
+            stab[self._qubit_index(self.n_level - 2, i)] = 1
+            stab[self._qubit_index(self.n_level - 2, i + 1)] = 1
+            stabilizers.append(stab)
+        for level in range(2, self.n_level - 2, 3):
+            for i in range(0, self._n_qubits_in_level(level), 2):
+                # stabilizer in the middle
+                stab = np.zeros((self.n_qubits,), dtype=np.dtype('b'))
+                stab[self._qubit_index(level, i)] = 1
+                stab[self._qubit_index(level, i + 1)] = 1
+                stab[self._qubit_index(level + 1, i)] = 1
+                stab[self._qubit_index(level + 1, i + 1)] = 1
+                stab[self._qubit_index(level + 2, i)] = 1
+                stab[self._qubit_index(level + 2, i + 1)] = 1
+                stabilizers.append(stab)
+
+        return np.array(stabilizers, dtype=np.dtype('b'))
 
     @property
     def flatXflips2Zerr(self):
-        _flatXflips2Zerr = np.zeros((2, self.n_qubits), dtype=np.dtype('b'))
-        for k in range(2 * self.L):  # vertical circle
-            _flatXflips2Zerr[0, k * self.W] = 1
-        for k in range(self.W): # horizontal circle
-            _flatXflips2Zerr[1, k] = 1
-            _flatXflips2Zerr[1, self.W + k] = 1
+        _flatXflips2Zerr = np.zeros((1, self.n_qubits), dtype=np.dtype('b'))
+        for level in range(self.n_level):
+            if self._is_qubit_on_right_edge_in_level(level):
+                _flatXflips2Zerr[0, self._qubit_index(level, 0)] = 1
         return _flatXflips2Zerr
 
     @property
     def flatZflips2Xerr(self):
         L = self.L
-        _flatZflips2Xerr = np.zeros((2, self.n_qubits), dtype=np.dtype('b'))
-        for k in range(self.W): # horizontal circle
-            _flatZflips2Xerr[0, k] = 1
-            _flatZflips2Xerr[1, self.W + k] = 1
-        for k in range(2 * self.L): # vertical circle
-            _flatZflips2Xerr[1, k * self.W] = 1
+        _flatZflips2Xerr = np.zeros((1, self.n_qubits), dtype=np.dtype('b'))
+        for level in range(self.n_level):
+            _flatZflips2Xerr[0, self._qubit_index(level, 0)] = 1
         return _flatZflips2Xerr
 
+    def _logical_operators(self):
+        logical_operators = np.zeros((1, self.n_qubits), dtype=np.dtype('b'))
+        for level in range(self.n_level):
+            if self._is_qubit_on_right_edge_in_level(level):
+                logical_operators[0, self._qubit_index(level, 0)] = 1
+        return logical_operators
+
     def H(self, Z=True, X=False):
+        stabilizers = self._stabilizers() # X and Z stabilizers are on the same qubits
         H = []
         if Z:
-            H.append(self.flatXflips2Zstab)
+            H.append(stabilizers.copy())
         if X:
-            H.append(self.flatZflips2Xstab)
+            H.append(stabilizers.copy())
         self.H = scipy.linalg.block_diag(*H)
         return self.H
 
     def E(self, Z=True, X=False):
+        logical_operators = self._logical_operators() # X and Z logical operators are on the same qubits
         E = []
         if Z:
-            E.append(self.flatXflips2Zerr)
+            E.append(logical_operators.copy())
         if X:
-            E.append(self.flatZflips2Xerr)
+            E.append(logical_operators.copy())
         E = scipy.linalg.block_diag(*E)
         return E
 
@@ -247,6 +322,4 @@ def Get_honeycomb_Code(L,full_H=False):
 
 #############################################
 if __name__ == "__main__":
-    Get_honeycomb_Code(4)
-    class Code:
-        pass
+    Get_honeycomb_Code(5)
